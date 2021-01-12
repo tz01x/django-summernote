a194f5cf39b3e45e109b7d3dcbce82eae5c6a79d
Park Hyun-woo <ez.amiryo@gmail.com>
2021-01-12 23:26:44 +0900

    Fix for codecov

--- django_summernote/__init__.py
+++ django_summernote/__init__.py
@@ -4,6 +4,10 @@ __version__ = version = '.'.join(map(str, version_info))
 __project__ = PROJECT = 'django-summernote'
 __author__ = AUTHOR = "django-summernote contributors"
 
-from django import VERSION as django_version
-if django_version < (3, 2):
-    default_app_config = 'django_summernote.apps.DjangoSummernoteConfig'
+try:
+    from django import VERSION as django_version
+    if django_version < (3, 2):
+        default_app_config = 'django_summernote.apps.DjangoSummernoteConfig'
+except ModuleNotFoundError:
+    # This part is needed for setup.py
+    pass
