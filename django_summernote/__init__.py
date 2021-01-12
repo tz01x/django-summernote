cdf674fc65491c72723d068e72f9ba9f85c5b482
Park Hyun-woo <ez.amiryo@gmail.com>
2021-01-12 23:05:34 +0900

    Fix default_app_config problem with Django >= 3.20

--- django_summernote/__init__.py
+++ django_summernote/__init__.py
@@ -4,5 +4,6 @@ __version__ = version = '.'.join(map(str, version_info))
 __project__ = PROJECT = 'django-summernote'
 __author__ = AUTHOR = "django-summernote contributors"
 
-
-default_app_config = 'django_summernote.apps.DjangoSummernoteConfig'
+from django import VERSION as django_version
+if django_version < (3, 2):
+    default_app_config = 'django_summernote.apps.DjangoSummernoteConfig'
