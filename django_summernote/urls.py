073eaaa80c13ddcee5cda3fede7b65247db165b6
Park Hyunwoo <ez.amiryo@gmail.com>
2021-02-13 00:41:10 +0900

    The url for uploaded attachments should not be added when disable_attachment is True

--- django_summernote/urls.py
+++ django_summernote/urls.py
@@ -10,3 +10,9 @@ urlpatterns = [
     path('editor/<id>/', SummernoteEditor.as_view(),
          name='django_summernote-editor'),
 ]
+
+if not get_config()['disable_attachment']:
+    urlpatterns += [
+        path('upload_attachment/', SummernoteUploadAttachment.as_view(),
+             name='django_summernote-upload_attachment'),
+    ]
